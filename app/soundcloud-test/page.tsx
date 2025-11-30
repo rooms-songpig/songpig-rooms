84a2a757d07fbf98876fdef0ee304645915cd8bd
songpig-frank <franklinleetx@gmail.com>
2025-11-30 09:47:36 -0600

    sound cloud player test page

--- app/soundcloud-test/page.tsx
+++ app/soundcloud-test/page.tsx
@@ -28,19 +28,26 @@ export default function SoundCloudTestPage() {
           textAlign: 'center',
         }}
       >
-        <h2 style={{ marginTop: 0, marginBottom: 16 }}>
-          Private Track Test (Security Override)
-        </h2>
+        <h2 style={{ marginTop: 0, marginBottom: 16 }}>Maximum Stealth Player</h2>
 
-        <iframe
-          width="100%"
-          height="166"
-          scrolling="no"
-          frameBorder="no"
-          allow="autoplay; encrypted-media"
-          sandbox="allow-scripts allow-same-origin"
-          src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/soundcloud%3Atracks%3A2221470425%3Fsecret_token%3Ds-ItCn8A5N42y&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"
-        />
+        <div
+          style={{
+            overflow: 'hidden',
+            height: 150,
+            borderRadius: 6,
+            border: '1px solid #1f2937',
+          }}
+        >
+          <iframe
+            width="100%"
+            height="166"
+            scrolling="no"
+            frameBorder="no"
+            allow="autoplay; encrypted-media"
+            sandbox="allow-scripts allow-same-origin"
+            src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/soundcloud%3Atracks%3A2221470425%3Fsecret_token%3Ds-ItCn8A5N42y&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=false&show_reposts=false&show_teaser=false&sharing=false&liking=false&show_playcount=false&show_artwork=false"
+          />
+        </div>
       </div>
     </main>
   );
